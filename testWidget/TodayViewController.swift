5480221bf9347eb398d9711f17ae30354029a029
takeru134 <murakwa.takeru@gmail.com>
2018-03-11 17:03:18 +0900

    はじめてのコミット

--- testWidget/TodayViewController.swift
+++ testWidget/TodayViewController.swift
@@ -1,16 +1,16 @@
 //
-//  ViewController.swift
-//  test
+//  TodayViewController.swift
+//  testWidget
 //
-//  Created by T.Murakawa on 2018/03/03.
+//  Created by T.Murakawa on 2018/03/07.
 //  Copyright © 2018年 takeru murakawa. All rights reserved.
 //
 
 import UIKit
+import NotificationCenter
 
-class ViewController: UIViewController {
-
-
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
     @IBOutlet var percentageOfMonth: UILabel!
     @IBOutlet var percentageOfDay: UILabel!
     @IBOutlet var percentageOfHour: UILabel!
@@ -23,19 +23,15 @@ class ViewController: UIViewController {
     private var day:Double = 0
     private var hour:Double = 0
     
-    private let pi = CGFloat(Double.pi)
-    private let start0:CGFloat = 0
-    private let end0 :CGFloat = 2 * CGFloat(Double.pi)
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.extensionContext?.widgetLargestAvailableDisplayMode = NCWidgetDisplayMode.expanded
+        
         self.getNowTime()
         
         self.percentageOfMonth.center = CGPoint(x:self.view.frame.width/4, y:self.view.frame.height/4)
         self.percentageOfDay.center = CGPoint(x:self.view.frame.width/2, y:self.view.frame.height/4)
         self.percentageOfHour.center = CGPoint(x:self.view.frame.width/4*3, y:self.view.frame.height/4)
-        
         self.view.bringSubview(toFront: percentageOfMonth)
         self.view.bringSubview(toFront: percentageOfDay)
         self.view.bringSubview(toFront: percentageOfHour)
@@ -43,9 +39,8 @@ class ViewController: UIViewController {
         self.monthLabel.center = CGPoint(x:self.view.frame.width/4, y:self.view.frame.height/4 + 50)
         self.dayLabel.center = CGPoint(x:self.view.frame.width/2, y:self.view.frame.height/4 + 50)
         self.hourLabel.center = CGPoint(x:self.view.frame.width/4*3, y:self.view.frame.height/4 + 50)
-
     }
-
+    
     func getNowTime(){
         print("getNowTime()")
         
@@ -57,7 +52,7 @@ class ViewController: UIViewController {
         let currentMonth = calendar.component(.month, from: Date())
         let currentDay = calendar.component(.day, from: Date())
         let currentHour = calendar.component(.hour, from: Date())
-
+        
         self.month = round(Double(currentMonth) / 12 * 100)
         self.day = round(Double(currentDay) / self.CalcLastDay() * 100)
         self.hour = round(Double(currentHour) / 24 * 100)
@@ -89,6 +84,9 @@ class ViewController: UIViewController {
     }
     
     func drawGraphPercentageOfMonth(num:Double){
+        let pi = CGFloat(M_PI)
+        var start0:CGFloat = 0 // 開始の角度
+        var end0 :CGFloat = 2 * pi// 終了の角度
         
         let path1: UIBezierPath = UIBezierPath();
         path1.move(to: CGPoint(x:self.view.frame.width/4, y:self.view.frame.height/4))
@@ -99,9 +97,9 @@ class ViewController: UIViewController {
         layer1.path = path1.cgPath
         
         self.view.layer.addSublayer(layer1)
-    
-        let start:CGFloat = 1.5 * pi // 開始の角度
-        let end :CGFloat = CGFloat(num)/100 * 2 * pi  - 0.5 * pi//- 1.5 * pi// 終了の角度
+        
+        var start:CGFloat = 1.5 * pi // 開始の角度
+        var end :CGFloat = CGFloat(num)/100 * 2 * pi  - 0.5 * pi//- 1.5 * pi// 終了の角度
         
         let path2: UIBezierPath = UIBezierPath();
         path2.move(to: CGPoint(x:self.view.frame.width/4, y:self.view.frame.height/4))
@@ -122,11 +120,14 @@ class ViewController: UIViewController {
         layer3.path = path3.cgPath
         
         self.view.layer.addSublayer(layer3)
-
+        
     }
     
     func drawGraphPercentageOfDay(num:Double){
-
+        let pi = CGFloat(M_PI)
+        var start0:CGFloat = 0 // 開始の角度
+        var end0 :CGFloat = 2 * pi// 終了の角度
+        
         let path1: UIBezierPath = UIBezierPath();
         path1.move(to: CGPoint(x:self.view.frame.width/2, y:self.view.frame.height/4))
         path1.addArc(withCenter: CGPoint(x:self.view.frame.width/2, y:self.view.frame.height/4), radius: 30, startAngle: start0, endAngle: end0, clockwise: true) // 円弧
@@ -137,8 +138,8 @@ class ViewController: UIViewController {
         
         self.view.layer.addSublayer(layer1)
         
-        let start:CGFloat = 1.5 * pi // 開始の角度
-        let end :CGFloat = CGFloat(num)/100 * 2 * pi  - 0.5 * pi//- 1.5 * pi// 終了の角度
+        var start:CGFloat = 1.5 * pi // 開始の角度
+        var end :CGFloat = CGFloat(num)/100 * 2 * pi  - 0.5 * pi//- 1.5 * pi// 終了の角度
         
         let path2: UIBezierPath = UIBezierPath();
         path2.move(to: CGPoint(x:self.view.frame.width/2, y:self.view.frame.height/4))
@@ -162,7 +163,10 @@ class ViewController: UIViewController {
     }
     
     func drawGraphPercentageOfHour(num:Double){
-
+        let pi = CGFloat(M_PI)
+        var start0:CGFloat = 0 // 開始の角度
+        var end0 :CGFloat = 2 * pi// 終了の角度
+        
         let path1: UIBezierPath = UIBezierPath();
         path1.move(to: CGPoint(x:self.view.frame.width/4*3, y:self.view.frame.height/4))
         path1.addArc(withCenter: CGPoint(x:self.view.frame.width/4*3, y:self.view.frame.height/4), radius: 30, startAngle: start0, endAngle: end0, clockwise: true) // 円弧
@@ -173,8 +177,8 @@ class ViewController: UIViewController {
         
         self.view.layer.addSublayer(layer1)
         
-        let start:CGFloat = 1.5 * pi // 開始の角度
-        let end :CGFloat = CGFloat(num)/100 * 2 * pi  - 0.5 * pi//- 1.5 * pi// 終了の角度
+        var start:CGFloat = 1.5 * pi // 開始の角度
+        var end :CGFloat = CGFloat(num)/100 * 2 * pi  - 0.5 * pi//- 1.5 * pi// 終了の角度
         
         let path2: UIBezierPath = UIBezierPath();
         path2.move(to: CGPoint(x:self.view.frame.width/4*3, y:self.view.frame.height/4))
@@ -196,18 +200,29 @@ class ViewController: UIViewController {
         
         self.view.layer.addSublayer(layer3)
     }
-
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(true)
+    
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+        if activeDisplayMode == NCWidgetDisplayMode.compact{
+            //compact
+            self.preferredContentSize = maxSize
+        }else{
+            self.preferredContentSize = CGSize(width:0, height:200)
+        }
     }
     
-    //メモリがやばいときに呼ばれるfunction
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
 }
-
